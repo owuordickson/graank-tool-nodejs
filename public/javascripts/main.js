bbc26b894dcd2da4fa8f9e7d6abec98da880bb50
owuordickson <owuordickson@gmail.com>
2019-10-29 12:43:37 +0100

    testing eventListeners

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,58 @@
+//const path = require('path')
+//const {ipcRenderer} = require('electron')
+//const tooltip = require('electron-tooltip')
+//const mime = require('mime')
+//const csvJson = require('csvtojson')
 
-var testClick = function(){
-    console.log("you clicked me");
+
+const selectPattern = document.querySelector('.dropdown-menu')
+const selectDirBtn = document.getElementById('select-directory')
+const uploadFile = document.getElementById('upload-file')
+const runPattern = document.getElementById('run-algorithm')
+const runPattern1 = document.getElementById('run-algorithm1')
+
+const msgLabel = document.getElementById('show-message')
+
+let gradualEP = false
+let file1 = ''
+let file2 = ''
+
+/*tooltip({
+    //
+})*/
+// ----------------------- Show main content -----------------------------
+
+function showMainContent(){
+  document.querySelector('.grid-content-right').classList.add('is-shown')
+  resultView = document.querySelector('.grid-content-left.is-shown')
+  if (resultView) resultView.classList.remove('is-shown')
+  console.log("display landing page")
 }
+
+showMainContent()
+
+//----------------------------------------------------------------------------
+
+
+selectPattern.addEventListener('click', (event) => {
+    if(event.target.dataset.value){
+      document.getElementById('pattern-type').innerHTML = `${event.target.dataset.value} Patterns`
+      //if (!gradualEP){
+      /*gradualEP = false
+      msgLabel.innerHTML = ''
+      closeResultContent()
+      closeProgress()
+      closeSpecifications()*/
+      //}
+      var txt = `${event.target.dataset.value} Patterns`
+      console.log(txt)
+    }
+  })
+
+runPattern1.addEventListener('click', function() {
+    console.log("added click event")
+});
+
+/*runPattern1.onclick = function(){
+    console.log("another added click event")
+}*/
