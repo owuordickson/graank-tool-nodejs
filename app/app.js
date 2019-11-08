b5b0d7c1762672a5b10a94ef831dd6be51753ce2
owuordickson <owuordickson@gmail.com>
2019-11-08 11:20:10 +0100

    added body-parser

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 
 var graankRouter = require('./routes/graank.route');
@@ -15,9 +16,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: false, limit: '100mb' }));
-//app.use(express.bodyParser({limit: '50mb'}));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
