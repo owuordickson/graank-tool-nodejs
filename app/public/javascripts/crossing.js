de7d525e1c206270fe7828c024f40cfa536c0858
owuordickson <owuordickson@gmail.com>
2020-03-04 16:56:53 +0300

    added

--- app/public/javascripts/crossing.js
+++ app/public/javascripts/crossing.js
@@ -1,13 +1,48 @@
 Dropzone.autoDiscover = false;
 
-const csvFiles = document.getElementById('csv-files')
+const uploads = document.getElementById('uploads')
+const crossFiles = document.getElementById('cross-files')
 
 function setupDropzone(){
-    var myDropzone = new Dropzone(csvFiles, {
-        url: "/x/runPython",
+    var myDropzone = new Dropzone(uploads, {
+        url: "/x/upload",
         acceptedFiles: 'text/csv',
         maxFilesize: 100
     });
 }
 
 setupDropzone()
+
+
+crossFiles.addEventListener('click', (event) => {
+    python_path = '../public/python_modules/src/init_crossing.py'
+    req = [python_path]
+    runPythonCode(req)
+});
+
+
+function runPythonCode(request){
+    var payload = JSON.stringify({data: request})
+    console.log("sending Python request")
+    var x = new XMLHttpRequest();
+    x.onreadystatechange = function(){
+      if( x.status === 200 && x.readyState === 4) {
+        // Optional callback for when request completes
+        var msg = JSON.parse(x.responseText);
+        console.log(msg);
+  
+        if (msg.success == 1){
+          //document.getElementById('text-result').innerHTML = `${msg.pyload}`
+          //showResultContent()
+          console.log(msg.pyload)
+        }else if (msg.success == 0){
+          //msgLabel.innerHTML = '<p>Sorry, an error occured. Check console for more details</p>'
+          console.log(msg.pyload)
+        }
+        //closeProgress()
+      }
+    }
+    x.open('POST', '/x/runPython', true);
+    x.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    x.send(payload);
+}
--- app/routes/crossing.route.js
+++ app/routes/crossing.route.js
@@ -1,34 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var spawn = require('child_process').spawn
+var path = require('path')
+
+var csvFiles = [];
 
 /* GET crossing csv files */
 router.get('/', function(req, res, next) {
   res.render('crossing', { title: 'fuzztx' });
 });
 
+router.post('/upload', function(req, res){
+  console.log("uploading");
+  csvFiles.push(req.files.file)
+  console.log("uploaded");
+  res.send("finished uploading");
+});
+
 router.post('/runPython', function(req, res){
   console.log("working");
-  console.log(req.body.data);
-  res.send("ok");
-  /*var py_req = req.body.data
-  var py_path = python_path = path.join(__dirname, py_req[0])
-  py_req[0] = py_path
-  const pythonProcess = spawn('python', req.body.data);
-  pythonProcess.stdout.on('data', (data) => {
-      // Do something with the data returned from python script
-      console.log("finished working");
-      var response = JSON.stringify({success: 1, pyload: data});
+  if (csvFiles.length >= 2){
+
+    var temp = {data: [req.body.data[0], csvFiles]}
+    //console.log(temp);
+
+    var py_req = temp.data
+    var py_path = python_path = path.join(__dirname, py_req[0])
+    py_req[0] = py_path
+    console.log(py_path)
+    /*const pythonProcess = spawn('python', req.body.data);
+    pythonProcess.stdout.on('data', (data) => {
+        // Do something with the data returned from python script
+        console.log("finished working");
+        var response = JSON.stringify({success: 1, pyload: data});
+        res.send(response);
+    });
+    pythonProcess.stderr.on('data', (data) => {
+      console.error("Error: ", data.toString());
+      var response = JSON.stringify({success: 0, pyload: data.toString()});
       res.send(response);
-  });
-  pythonProcess.stderr.on('data', (data) => {
-    console.error("Error: ", data.toString());
-    var response = JSON.stringify({success: 0, pyload: data.toString()});
+    })
+    pythonProcess.on('close', (code) => {
+      console.log("Child exited with code ", code);
+    })*/
+  }else{
+    console.error("Error: Upload at least 2 csv files");
+    var response = JSON.stringify({success: 0, pyload: "Upload at least 2 csv files"});
     res.send(response);
-  })
-  pythonProcess.on('close', (code) => {
-    console.log("Child exited with code ", code);
-  })*/
+  }
 
 });
 
